df680b8f4aad0598554e9175f108eaf552e22765
unknown <you@example.com>
2020-09-02 17:24:45 +0800

    elm

---
+++ VUE/vue-elm/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <v-header :seller="seller"></v-header>
+    <!-- header 组件 -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import VHeader from '@/components/v-header/v-header.vue';
+import { getSeller } from '@/api';
+import qs from 'query-string'
+
+export default {
+  data() {
+    return {
+      seller: {
+        id: qs.parse(location.search).id
+      },
+    };
+  },
+  created() {
+    this._getSeller();
+  },
+  methods: {
+    _getSeller() {
+      getSeller({
+        id: this.seller.id,
+      }).then((seller) => {
+        this.seller = Object.assign({}, this.seller, seller)
+        // console.log(this.seller);
+      });
+    },
+  },
+  components: {
+    VHeader,
+  },
+};
+</script>>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ VUE/vue-elm/vue.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  lintOnSave: 'warning',
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        import: [
+          './src/theme',
+        ],
+      },
+    },
+  },
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true,
+    },
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://ustbhuangyi.com/sell/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/api'
+        }
+      }
+    }
+  }
+};
